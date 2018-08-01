ac19b333026dc18ccf710b725b2d2b4cd702038e
Dima Nazdratenko <d.nazdratenko@lucky-labs.com>
2018-08-01 23:22:09 +0300

    lesson 0, part 1

---
+++ source/basic-redux/bus/gallery/actions.js
@@ -0,0 +1,10 @@
+// Types
+import { SHOW_PREVIOUS_PHOTO, SHOW_NEXT_PHOTO } from "./types";
+
+export const showPreviousPhoto = {
+    type: SHOW_PREVIOUS_PHOTO,
+};
+
+export const showNextPhoto = {
+    type: SHOW_NEXT_PHOTO,
+};
