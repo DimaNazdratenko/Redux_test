112a9873ef7795c93a30c271a94602b5f145d2df
d.nazdratenko <dnazdratenko@warfare-tec.com>
2018-08-03 14:09:06 +0300

    lesson 1, home work

--- source/bus/posts/actions.js
+++ source/bus/posts/actions.js
@@ -1,5 +1,5 @@
 // Types
-import { FETCH_POSTS_ASYNC, FILL_POSTS } from "./types";
+import { FETCH_POSTS_ASYNC, FILL_POSTS, CREATE_POST_ASYNC, CREATE_POST } from "./types";
 
 // Instruments
 import { api } from "../../REST";
@@ -20,4 +20,22 @@ export const fetchPostsAsync = () => async (dispatch) => {
     const result = await response.json();
 
     dispatch(fillPosts(result.data));
+};
+
+export const createPost = (post) => {
+    return {
+        type:    CREATE_POST,
+        payload: post,
+    };
+};
+
+export const createPostAsync = (comment) => async (dispatch) => {
+    dispatch({
+        type: CREATE_POST_ASYNC,
+    });
+
+    const response = await api.posts.create(comment);
+    const result = await response.json();
+
+    dispatch(createPost(result.data));
 };
