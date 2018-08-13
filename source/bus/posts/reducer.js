c12d168708fa708263a7242546f81b95297b03f2
Dima Nazdratenko <d.nazdratenko@lucky-labs.com>
2018-08-13 19:10:14 +0300

    lesson 5, home work, part 2

--- source/bus/posts/reducer.js
+++ source/bus/posts/reducer.js
@@ -14,17 +14,8 @@ export const postsReducer = (state = initialState, action) => {
         case types.CREATE_POST:
             return state.unshift(fromJS(action.payload));
 
-        // case types.REMOVE_POST:
-        //     console.log('~~~~~~~~ state: ', state);
-        //
-        //     return state.filter((post) => {
-        //         console.log('~~~~~~~~ post: ', post._root.entries[0][1]);
-        //         // console.log('~~~~~~~~ post.id: ', post.id);
-        //         console.log('~~~~~~~~ action.payload: ', action.payload);
-        //         console.log('~~~~~~~~ post.id != action.payload: ', post._root.entries[0][1] != action.payload);
-        //
-        //         return post._root.entries[0][1] != action.payload
-        //     });
+        case types.REMOVE_POST:
+            return state.filter((post) => post.get('id') !== action.payload);
 
         case types.CLEAR_POSTS:
             return state.clear();
