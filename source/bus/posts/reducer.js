b1116744cdee3eefdad9554083d75037ec8bcbcd
d.nazdratenko <dnazdratenko@warfare-tec.com>
2018-08-16 10:38:40 +0300

    lesson 6, part 1

--- source/bus/posts/reducer.js
+++ source/bus/posts/reducer.js
@@ -15,7 +15,20 @@ export const postsReducer = (state = initialState, action) => {
             return state.unshift(fromJS(action.payload));
 
         case types.REMOVE_POST:
-            return state.filter((post) => post.get('id') !== action.payload);
+            return state.filter((post) => post.get("id") !== action.payload);
+
+        case types.LIKE_POST:
+            return state.updateIn(
+                [
+                    state.findIndex((post) => {
+                        return post.get("id") === action.payload.postId;
+                    }),
+                    "likes"
+                ],
+                (likes) => {
+                    return likes.unshift(action.payload.liker);
+                }
+            );
 
         case types.CLEAR_POSTS:
             return state.clear();
--- source/bus/posts/saga/watchers.js
+++ source/bus/posts/saga/watchers.js
@@ -5,20 +5,26 @@ import { takeEvery, all, call } from "redux-saga/effects";
 import { types } from "../types";
 
 // Workers
-import { createPost, removePost, fillPosts } from "./workers";
+import { createPost, removePost, fillPosts, likePost } from "./workers";
 
 function* watchCreatePost () {
     yield takeEvery(types.CREATE_POST_ASYNC, createPost);
 }
-
 function* watchRemovePost () {
     yield takeEvery(types.REMOVE_POST_ASYNC, removePost);
 }
-
 function* watchFillPosts () {
     yield takeEvery(types.FETCH_POSTS_ASYNC, fillPosts);
 }
+function* watchLikePost () {
+    yield takeEvery(types.LIKE_POST_ASYNC, likePost);
+}
 
 export function* watchPosts () {
-    yield all([call(watchCreatePost), call(watchRemovePost), call(watchFillPosts)]);
+    yield all([
+        call(watchCreatePost),
+        call(watchRemovePost),
+        call(watchFillPosts),
+        call(watchLikePost)
+    ]);
 }
