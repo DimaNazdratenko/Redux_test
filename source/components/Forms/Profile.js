efc57a9a29133eadefe713553a00dc820555aa34
d.nazdratenko <dnazdratenko@warfare-tec.com>
2018-08-29 11:17:02 +0300

    lesson 8, part 3

--- source/components/Forms/Profile.js
+++ source/components/Forms/Profile.js
@@ -15,7 +15,7 @@ import { book } from "../../navigation/book";
 import { Input } from "../../components";
 
 // Actions
-// import { authActions } from "../../bus/auth/actions";
+import { profileActions } from "../../bus/profile/actions";
 
 const mapStateToProps = (state) => {
     return {
@@ -24,18 +24,15 @@ const mapStateToProps = (state) => {
     };
 };
 
-// const mapDispatchToProps = {
-//     loginAsync: authActions.loginAsync,
-// };
+const mapDispatchToProps = profileActions;
 
 @connect(
-    mapStateToProps
-    // mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )
 export default class Profile extends Component {
     static defaultProps = {
         // Actions
-        updateNameAsync:   () => {},
         updateAvatarAsync: () => {},
     };
 
