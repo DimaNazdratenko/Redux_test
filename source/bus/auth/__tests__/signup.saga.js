6fa9f828d57335954839d5ca7e652a0b0cde2864
d.nazdratenko <dnazdratenko@warfare-tec.com>
2018-09-06 10:26:52 +0300

    lesson 11, part 3

--- source/bus/auth/__tests__/signup.saga.js
+++ source/bus/auth/__tests__/signup.saga.js
@@ -41,13 +41,35 @@ describe("signup saga:", () => {
         });
 
         test("should dispatch 'stopFetching' action", () => {
-            expect(clone.next().value).toEqual(
-                put(uiActions.stopFetching())
-            );
+            expect(clone.next().value).toEqual(put(uiActions.stopFetching()));
         });
 
         test("should finish", () => {
             expect(clone.next().done).toBe(true);
+        });
+    });
+
+    describe("should handle a 200 status response", () => {
+        test("a fetch request should return a 200 status response data object", () => {
+            expect(saga.next(__.fetchResponseSuccess).value).toEqual(
+                apply(__.fetchResponseSuccess, __.fetchResponseSuccess.json)
+            );
+        });
+
+        test("should dispatch 'fillProfile' action", () => {
+            expect(saga.next(__.responseDataSuccess).value).toMatchSnapshot();
+        });
+
+        test("should dispatch 'authenticate' action", () => {
+            expect(saga.next().value).toMatchSnapshot();
+        });
+
+        test("should dispatch 'stopFetching' action", () => {
+            expect(saga.next().value).toMatchSnapshot();
+        });
+
+        test("should finish", () => {
+            expect(saga.next().done).toBe(true);
         });
     });
 });
